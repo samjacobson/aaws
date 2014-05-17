3c92e8eda316fdc4662283163bad3041684242c4
Sam Jacobson <sam@eversoft.co.nz>
2014-05-17 13:16:35 +1200

    added CreatePlatformEndpoint function, and updated Publish to use TargetArn which is more flexible than using TopicArn

--- aaws/sns.py
+++ aaws/sns.py
@@ -119,6 +119,61 @@ class SNS(AWSService):
 			r.addParm('AuthenticateOnUnsubscribe', 'Yes')
 		return r
 
+	def CreatePlatformEndpoint(self, PlatformApplicationArn, Token, CustomUserData=None, Attributes=None):
+		"""Creates an endpoint for a device and mobile app on one of the supported push notification services,
+			such as GCM and APNS. CreatePlatformEndpoint requires the PlatformApplicationArn that is returned from
+			CreatePlatformApplication. The EndpointArn that is returned when using CreatePlatformEndpoint can then
+			be used by the Publish action to send a message to a mobile app or by the Subscribe action for
+			subscription to a topic.
+
+			The CreatePlatformEndpoint action is idempotent, so if the requester already owns an endpoint with the
+			same device token and attributes, that endpoint's ARN is returned without creating a new endpoint.
+			For more information, see Using Amazon SNS Mobile Push Notifications.
+
+			PlatformApplicationArn -- PlatformApplicationArn returned from CreatePlatformApplication is used to
+										create an endpoint.
+				Type: String
+				Required: Yes
+
+			Token -- Unique identifier created by the notification service for an app on a device. The specific
+						name for Token will vary, depending on which notification service is being used.
+						For example, when using APNS as the notification service, you need the device token.
+						Alternatively, when using GCM or ADM, the device token equivalent is called the registration ID.
+				Type: String
+				Required: Yes
+
+			CustomUserData - Arbitrary user data to associate with the endpoint. Amazon SNS does not use this data.
+								The data must be in UTF-8 format and less than 2KB.
+				Type: String
+				Required: No
+
+			Attributes - For a list of attributes, see SetEndpointAttributes.
+				Type: enumerable of (key, value) Strings. Can also be a dict of String->String
+				Required: No
+		"""
+
+		def response(status, reason, data):
+			if status == 200:
+				root = ET.fromstring(data)
+				node = root.find('.//{http://sns.amazonaws.com/doc/2010-03-31/}EndpointArn')
+				if node is not None:
+					return node.text
+			raise AWSError(status, reason, data)
+
+		r = request.AWSRequest(self._endpoint, '/', self._key, self._secret, 'CreatePlatformEndpoint', {
+				'PlatformApplicationArn': PlatformApplicationArn,
+				'Token': Token,
+			}, response)
+		if CustomUserData is not None:
+			r.addParm('CustomUserData', CustomUserData)
+		if Attributes is not None:
+			if hasattr(Attributes, 'items'):
+				Attributes = Attributes.items()
+			for idx, (key, value) in enumerate(Attributes):
+				r.addParm('Attributes.entry.%d.key' % (idx + 1), str(key))
+				r.addParm('Attributes.entry.%d.value' % (idx + 1), str(value))
+		return r
+
 
 	def CreateTopic(self, Name):
 		"""The CreateTopic action creates a topic to which notifications can be published.
@@ -316,13 +371,13 @@ class SNS(AWSService):
 			}, response, request.ListFollow)
 
 
-	def Publish(self, TopicArn, Message, Subject=None, MessageStructure=None):
+	def Publish(self, TargetArn, Message, Subject=None, MessageStructure=None):
 		"""The Publish action sends a message to all of a topic's subscribed endpoints. When a
 			messageId is returned, the message has been saved and Amazon SNS will attempt to deliver
 			it to the topic's subscribers shortly. The format of the outgoing message to each subscribed
 			endpoint depends on the notification protocol selected.
 
-			TopicArn -- The topic you want to publish to.
+			TargetArn -- The topic or endpoint you want to publish to.
 				Type: String
 				Required: Yes
 
@@ -372,7 +427,7 @@ class SNS(AWSService):
 			raise AWSError(status, reason, data)
 
 		return request.AWSRequest(self._endpoint, '/', self._key, self._secret, 'Publish', {
-				'TopicArn': TopicArn,
+				'TargetArn': TargetArn,
 				'Message': Message,
 				'MessageStructure': MessageStructure,
 				'Subject': Subject,
